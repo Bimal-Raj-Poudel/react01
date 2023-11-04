151ca6c3d747312fff6972450dbbf029bf6b0a34
C:/Users/Bhusal <startertechz@gmail.com>
2023-11-04 10:15:56 +0545

    started

---
+++ vite project/viteproj01/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import Home from "./component/Home"
+import Services from "./component/Services"
+import About from "./component/About"
+function App() {
+  
+
+  return (
+    <>
+     <Home/>
+     <About/>
+     <Services/>
+    </>
+  )
+}
+
+export default App
